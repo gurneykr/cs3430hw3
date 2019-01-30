99e1ce68948446ed2e17594d6efee2208b805b62
Krista Gurney <gurneykri@gmail.com>
2019-01-29 20:54:35 -0700

    add the deriv for quotient and product rule

--- deriv.py
+++ deriv.py
@@ -10,6 +10,7 @@ from const import const
 from pwr import pwr
 from prod import prod
 from plus import plus
+from quot import quot
 from maker import make_const, make_pwr, make_pwr_expr
 import math
 
@@ -93,14 +94,9 @@ def prod_deriv(p):
                 return const(0)
             else:
                 return prod(m1, deriv(m2))
-        # elif isinstance(m2, pwr):  # (1+x)*(2^3)
-        #     pass
-        # elif isinstance(m2, plus):  # (1+x)*(x+3)
-        #     pass
-        # elif isinstance(m2, prod):  # (3+x)*(2x)
-        #     pass
+        #product rule fg = f*g' + g*f'
         else:
-            raise Exception('prod_deriv: case 1:' + str(p))
+            return plus(prod(m1, deriv(m2)),prod(m2, deriv(m1)))
     elif isinstance(m1, pwr):
         if isinstance(m2, const):  # (x^2)*3 => (2x^1)*3
             if isinstance(deriv(m2), const):
@@ -109,7 +105,10 @@ def prod_deriv(p):
                 return prod(deriv(m1), m2)
         else:
             raise Exception('prod_deriv: case 2:' + str(p))
-
+        # pwr and sum (x^3)(x+2)
+        # pwr pwr (x^3)(x^3)
+        # pwr prod (x^3)(3x)
+        # pwr quotient (x^3)*(x-1)/(x+8)
     elif isinstance(m1, prod):
         if isinstance(m2, const):#(3x)*4
             if isinstance(deriv(m2), const):
@@ -118,5 +117,17 @@ def prod_deriv(p):
                 return prod(deriv(m1), m2)
         else:
             return prod(m1, deriv(m2))
+    #prod  sum (3x)(x+1)
+    #prod pwr (4x)(x^3)
+    #prod prod(4x)(3x)
+    #prod quotient (4x)*(x-1)/(x+8)
+    elif isinstance(m1, quot):# f/g = (gf'-fg')/g^2 quotient rule
+        #(m2m1' - m1m2')/m2^2
+        return quot( plus(prod(m2, deriv(m1)), prod(const(-1.0),prod(m1, deriv(m2)))),  pwr(m2, const(2.0)))
+
+    #quotient and sum (x-1)/(x+8) * (x+4)
+    #quotient and pwr (x-1)/(x+8) * (x^2)
+    #quotient and prod (x-1)/(x+8) * (3x)
+    #quotient and quotient (x-1)/(x+8) * (x-1)/(x+6)
     else:
        raise Exception('prod_deriv: case 4:' + str(p))
---
+++ deriv_test.py
@@ -0,0 +1,53 @@
+from var import var
+from const import const
+from pwr import pwr
+from prod import prod
+from plus import plus
+from quot import quot
+from maker import make_const, make_pwr, make_pwr_expr, make_plus, make_prod
+from tof import tof
+from deriv import deriv
+import unittest
+import math
+
+class Assign01UnitTests(unittest.TestCase):
+    def test_01(self):
+        print('\n***** Test 01 ***********')
+        e1 = make_plus(make_pwr('x', 1.0), make_const(1.0))
+        e2 = make_pwr('x', 3.0)
+        e3 = make_prod(make_const(5.0), make_pwr('x', 1.0))
+        e4 = make_plus(e2, e3)
+        e5 = make_plus(e4, make_const(2.0))
+        e6 = make_prod(e1, e5)
+        drv = deriv(e6)
+        assert not drv is None
+        print('-- derivative is:\n')
+        print(e6)
+        print(drv)
+        # e6f = tof(drv)
+        # assert not e6f is None
+        # gt = lambda x: 4.0 * (x ** 3) + 3 * (x ** 2) + 10.0 * x + 7.0
+        # print('\n Comparison with ground truth:\n')
+        # err = 0.00001
+        # for i in range(10):
+        #     print(e6f(i), gt(i))
+        #     assert abs(e6f(i) - gt(i)) <= err
+        print('Test 01:pass')
+
+    def test_02(self):
+        print('\n***** Test 02 ***********')
+        e1 = make_prod(make_const(2.0), make_pwr('x', 4.0))
+        e2 = make_prod(make_const(-1.0), make_pwr('x', 1.0))
+        e3 = make_plus(e1, e2)
+        e4 = make_plus(e3, make_const(1.0))
+        e5 = make_prod(make_const(-1.0), make_pwr('x', 5.0))
+        e6 = make_plus(e5, make_const(1.0))
+        e7 = make_prod(e4, e6)
+        print('-- function expression is:\n')
+        print(e7)
+        drv = deriv(e7)
+        print(drv)
+
+
+if __name__ == "__main__":
+        unittest.main()
