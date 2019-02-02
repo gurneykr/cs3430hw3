dd75d893905f1c7a8a71b722ee4c1d6656e15cda
Krista Gurney <gurneykri@gmail.com>
2019-02-01 17:07:06 -0700

    added more tests

--- deriv_test.py
+++ deriv_test.py
@@ -29,39 +29,37 @@ class Assign01UnitTests(unittest.TestCase):
         gt = lambda x: 4.0 * (x ** 3) + 3 * (x ** 2) + 10.0 * x + 7.0
         print('\n Comparison with ground truth:\n')
         err = 0.00001
-        # for i in range(10):
-        value = e6f(0)
-        print(value, gt(0))
-        assert abs(e6f(0) - gt(0)) <= err
+        for i in range(10):
+            value = e6f(i)
+            print(value, gt(i))
+            assert abs(e6f(i) - gt(i)) <= err
         print('Test 01:pass')
         #(x+1)(3x^2+5) + (x^3+5x+2)
 
-    #def test_04(self):
-        #e1 = plus(make_pwr('x', 1.0), make_const(1.0))
-        #e2 = plus(prod(make_const(3.0), make_pwr('x', 2.0)), make_const(5.0))
-        #e3 = prod(e1, e2)
-        #
-        # e4 = plus(make_pwr('x', 3.0), prod(make_const(5.0), make_pwr('x', 1.0)))
-        # e5 = plus(e4, make_const(2.0))
-        # e6 = plus(e3, e5)
-        #e6f = tof(e2)
-        #print("f(x): ",e3)
-        #print(e6f(0))
-
-    # def test_02(self):
-    #     print('\n***** Test 02 ***********')
-    #     e1 = make_prod(make_const(2.0), make_pwr('x', 4.0))
-    #     e2 = make_prod(make_const(-1.0), make_pwr('x', 1.0))
-    #     e3 = make_plus(e1, e2)
-    #     e4 = make_plus(e3, make_const(1.0))
-    #     e5 = make_prod(make_const(-1.0), make_pwr('x', 5.0))
-    #     e6 = make_plus(e5, make_const(1.0))
-    #     e7 = make_prod(e4, e6)
-    #     print('-- function expression is:\n')
-    #     print(e7)
-    #     drv = deriv(e7)
-    #     print(drv)
-    #
+    def test_02(self):
+        print('\n***** Test 02 ***********')
+        e1 = make_prod(make_const(2.0), make_pwr('x', 4.0))
+        e2 = make_prod(make_const(-1.0), make_pwr('x', 1.0))
+        e3 = make_plus(e1, e2)
+        e4 = make_plus(e3, make_const(1.0))
+        e5 = make_prod(make_const(-1.0), make_pwr('x', 5.0))
+        e6 = make_plus(e5, make_const(1.0))
+        e7 = make_prod(e4, e6)
+        print('-- function expression is:\n')
+        print(e7)
+        drv = deriv(e7)
+        assert not drv is None
+        print('\n - - derivative is:\n')
+        print(drv)
+        e7f = tof(drv)
+        assert not e7f is None
+        gt = lambda x: -18.0 * (x ** 8) + 6.0 * (x ** 5) - 5.0 * (x ** 4) + 8.0 * (x ** 3) - 1.0
+        err = 0.00001
+        print('n - -comparison with ground truth:\n')
+        for i in range(10):
+            print(e7f(i), gt(i))
+            assert abs(e7f(i) - gt(i)) <= err
+        print('Test 02: pass')
     # def test_03(self):
     #     print('\n *****Test03 ***********')
     #     q = make_quot(make_plus(make_pwr('x', 1.0), make_const(11.0)), make_plus(
@@ -71,7 +69,26 @@ class Assign01UnitTests(unittest.TestCase):
     #     print(pex)
     #     pexdrv = deriv(pex)
     #     print(pexdrv)
+    def test_03(self):
+        print('\n***** Test 03 ***********')
+        q = make_quot(make_plus(make_pwr('x', 1.0), make_const(11.0)), make_plus(make_pwr('x', 1.0), make_const(-3.0)))
+        pex = make_pwr_expr(q, 3.0)
+        print('-- function expression is:\n')
+        print(pex)
+        pexdrv = deriv(pex)
+        assert not pexdrv is None
+        print('\n - - derivative is:\n')
+        print(pexdrv)
+        pexdrvf = tof(pexdrv)
+        assert not pexdrvf is None
+        gt = lambda x: -42.0 * (((x + 11.0) ** 2) / ((x - 3.0) ** 4))
+        err = 0.00001
+        print('\n - -comparison with ground truth:\n')
+        for i in range(10):
+            if i != 3.0:
+                print(pexdrvf(i), gt(i))
+                assert abs(pexdrvf(i) - gt(i)) <= 0.001
+        print('Test 03:pass')
 
-
-if __name__ == "__main__":
+    if __name__ == "__main__":
         unittest.main()
